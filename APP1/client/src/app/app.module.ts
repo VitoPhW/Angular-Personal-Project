eb5ef23dc32d8512783bbb7b5d1635c74f53e972
VitoPhW <vitoph@yandex.com>
2022-01-15 21:21:13 +0200

    New components-lists,memres,messages + routing cnf

--- APP1/client/src/app/app.module.ts
+++ APP1/client/src/app/app.module.ts
@@ -11,13 +11,21 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    ListsComponent,
+    MessagesComponent,
+    MemberListComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
